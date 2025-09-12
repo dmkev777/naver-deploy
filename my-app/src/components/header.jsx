b92d0a6c12a6728967e654909d06ed1f3a3fb7e2
Pham Duc Minh <dminhkhtn07@gmail.com>
2025-09-13 00:00:56 +0700

    add header

---
+++ my-app/src/components/header.jsx
@@ -0,0 +1,11 @@
+import "./header.css";
+
+function Header() {
+    return (
+        <div className="header">
+            <div className="hello-user"> Hello </div>
+            <div className="motivate">Discipline is key</div>
+        </div>
+    )
+}
+export default Header;
