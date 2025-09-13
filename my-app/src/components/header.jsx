46f0f3627415662f6d0a265fc74e496a2447a8e9
Pham Duc Minh <dminhkhtn07@gmail.com>
2025-09-13 09:05:05 +0700

    modify sidebar and header color/text

--- my-app/src/components/header.jsx
+++ my-app/src/components/header.jsx
@@ -3,7 +3,7 @@ import "./header.css";
 function Header() {
     return (
         <div className="header">
-            <div className="hello-user"> Hello </div>
+            <div className="hello-user"> Welcome back! </div>
             <div className="motivate">Discipline is key</div>
         </div>
     )
