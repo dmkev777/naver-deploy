f493762d817e12a8e40b7877d60abe3c6e342b8c
Pham Duc Minh <dminhkhtn07@gmail.com>
2025-09-14 00:09:59 +0700

    build the basic of a todo list

--- my-app/src/components/TodoList.jsx
+++ my-app/src/components/TodoList.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import { useState } from 'react'
 import './TodoList.css'
-
+import remove_icon from '../assets/remove_icon.svg'
+import add_icon from '../assets/add_icon.svg'
 
 function TaskCard({task, id, onRemove}) {
     return <div className='taskcard'>
-        <p>Công việc: {task} - {id}</p>
-        <button onClick={()=> onRemove(id)}>Remove</button>
+        <p className='task-info'>Công việc: {task} - {id}</p>
+        <button className='remove-button' onClick={()=> onRemove(id)}> <img className='remove-icon' src={remove_icon}/> </button>
     </div>
 }
 
@@ -28,14 +29,14 @@ function TodoList() {
 
   return (
     <div className='todo-scroll'>
-    <form onSubmit={handleAdd}>
-        <input value={text} onChange={e => setTexts(e.target.value)} placeholder='Add task'/>
-        <button type='submit'>
-        Add
+    <form className='add-row' onSubmit={handleAdd}>
+        <input className='input-bar' value={text} onChange={e => setTexts(e.target.value)} placeholder='Add task'/>
+        <button className='add-button' type='submit'>
+        <img className='add-icon' src={add_icon}/>
         </button >
     </form>
     <div>
-        {tasks.length === 0 ?  (<div>No task to do</div>) : 
+        {tasks.length === 0 ?  (<div className='notasktodo'>No task to do</div>) : 
         ( tasks.map(task => 
         <TaskCard key={task.id} task={task.text} id={task.id}  onRemove={handleRemove}/>)
         )
